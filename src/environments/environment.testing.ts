bdf3c967d2b35585f8d40b7846bbc09fa3edf576
jenafrank <jenafrank@gmail.com>
2020-01-11 22:07:59 +0100

    Add testing environment to test on azure, i.e. use app with test db

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,17 @@
+export const environment = {
+
+  production: true,
+
+  firebase: {
+
+    apiKey: "AIzaSyBmktx9jP1C8rJrKRUNCJKHZgcix2LEvQc",
+    authDomain: "gutblatt-dev.firebaseapp.com",
+    databaseURL: "https://gutblatt-dev.firebaseio.com",
+    projectId: "gutblatt-dev",
+    storageBucket: "gutblatt-dev.appspot.com",
+    messagingSenderId: "519054181816",
+    appId: "1:519054181816:web:f354c5842c6fc83f53bb4d"
+
+  }
+
+};
